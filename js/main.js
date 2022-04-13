6d2fafce2e6e4c05e002f201fc76c384db2342d9
KamoliddinovDilshod <dilshodkamoliddinov03@gmail.com>
2022-04-13 23:57:37 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+const colRed = document.querySelector(".red");
+const colYellow = document.querySelector(".yellow");
+const colGreen = document.querySelector(".green");
+
+setInterval( () => {
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "red";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "black";
+  }, 1000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 3000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "black";
+  }, 3500);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 4000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "black";
+  }, 4500);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 5000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "green";
+  }, 5500);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 6000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "black";
+  }, 6500);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 7000);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "black";
+    colGreen.style.backgroundColor = "black";
+  }, 7500);
+
+  setTimeout(() => {
+    colRed.style.backgroundColor = "black";
+    colYellow.style.backgroundColor = "yellow";
+    colGreen.style.backgroundColor = "black";
+  }, 8000);
+
+}, 8500)
